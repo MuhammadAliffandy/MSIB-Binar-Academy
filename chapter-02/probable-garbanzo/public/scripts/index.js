56a58e35fecfb2d52b39c1e2505e82200cdc2443
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-09-09 00:30:09 +0700

    update DOM UI Manipulation

--- chapter-02/probable-garbanzo/public/scripts/index.js
+++ chapter-02/probable-garbanzo/public/scripts/index.js
@@ -1,5 +1,10 @@
+const wrapperBlur = document.getElementById("wrapperBlur");
+const navbar = document.getElementById("navbar");
 const pickDriverButton = document.getElementById('pickDriver');
 const pickTimeButton = document.getElementById('pickTime');
+const pickDateButton = document.getElementById('pickDate');
+const pickCountPersonButton = document.getElementById('pickCountPerson');
+const searchCarButton = document.getElementById('searchCarButton');
 const selectOptions = document.querySelectorAll('.select-options');
 let pickDriverValue = document.getElementById('pickDriver').value = '';
 let pickTimeValue =  document.getElementById("pickTime").value = '';
@@ -7,11 +12,31 @@ let pickTimeValue =  document.getElementById("pickTime").value = '';
 pickDriverButton.addEventListener('click', function () {
     selectOptions[0].style.display = selectOptions[0].style.display === 'block' ? 'none' : 'block';
     document.querySelectorAll('.select-styled')[0].style.border = '2px solid #5CB85F';
+    wrapperBlur.classList.add("wrapper-blur");
+    navbar.style.cssText = 'z-index: 0 !important;'
 });
 
 pickTimeButton.addEventListener('click', function () {
     selectOptions[1].style.display = selectOptions[1].style.display === 'block' ? 'none' : 'block';
     document.querySelectorAll('.select-styled')[1].style.border = '2px solid #5CB85F';
+    wrapperBlur.classList.add("wrapper-blur");
+    navbar.style.cssText = 'z-index: 0 !important;'
+    
+});
+
+pickDateButton.addEventListener('focus', function () {
+    wrapperBlur.classList.add("wrapper-blur");
+    navbar.style.cssText = 'z-index: 0 !important;'
+});
+
+pickCountPersonButton.addEventListener('click', function () {
+    wrapperBlur.classList.add("wrapper-blur");
+    navbar.style.cssText = 'z-index: 0 !important;'
+});
+
+searchCarButton.addEventListener('click', function () {
+    wrapperBlur.classList.remove("wrapper-blur");
+    navbar.style.zIndex = '1 !important';
 });
 
 selectOptions[0].addEventListener('click', function (e) {
@@ -23,7 +48,6 @@ selectOptions[0].addEventListener('click', function (e) {
     }
 });
 selectOptions[1].addEventListener('click', function (e) {
-    
     if (e.target.tagName === 'LI') {
         const selectedValue = e.target.textContent;
         document.querySelectorAll('.select-styled')[1].textContent = `${selectedValue.slice(0,2)}:00 WIB`;
@@ -38,9 +62,17 @@ document.addEventListener('click', function (e) {
         document.querySelectorAll('.select-styled')[0].style.border = '2px solid #ccc';
     }
 });
+
 document.addEventListener('click', function (e) {
     if (!pickTimeButton.contains(e.target)) {
         selectOptions[1].style.display = 'none';
         document.querySelectorAll('.select-styled')[1].style.border = '2px solid #ccc';
+    }
+});
+
+document.addEventListener('click', function (e) {
+    if(e.target.classList[0] == "wrapper-blur"){
+        wrapperBlur.classList.remove("wrapper-blur");
+        navbar.style.cssText = 'z-index: 10 !important;'
     }
 });
