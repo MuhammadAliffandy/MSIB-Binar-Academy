aab95269c4689a604e488451565222bad5730b23
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-11-03 22:10:09 +0700

    updated random time generator

--- chapter-06/binar-car/src/lib/randomTime.jsx
+++ chapter-06/binar-car/src/lib/randomTime.jsx
@@ -1,12 +1,15 @@
 export const generateRandomTime = () =>  {
-        const startTime = new Date(2023, 11, 1);
+
+        var month = new Date().getMonth() + 1; 
+
+        const startTime = new Date(2023, month, 1);
         startTime.setHours(8, 0, 0, 0);
-    
-        const endTime = new Date(2023, 11, 25);
+
+        const endTime = new Date(2023, month, 25);
         endTime.setHours(12, 0, 0, 0);
-    
+
         const timeDifference = endTime - startTime;
         const randomTime = new Date(startTime.getTime() + Math.random() * timeDifference);
-    
+
         return randomTime.toISOString();
 }
